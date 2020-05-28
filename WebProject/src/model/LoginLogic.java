06626420f1fbfe90f3c6473fcd4dd47255edc66a
kanagawa <kanagawa@MP613.mzdomain.maize.co.jp>
2020-05-28 11:31:55 +0900

    login処理を部分的に実装

--- WebProject/src/model/LoginLogic.java
+++ WebProject/src/model/LoginLogic.java
@@ -1,10 +1,22 @@
 package model;
 
+import java.sql.PreparedStatement;
+import java.util.List;
 
+import dao.MemberDAO;
 public class LoginLogic {
 
-	//DBに問い合わせてログイン可かどうか調べる
-	public static boolean login(String userId,String password) {
-		return true;
+	//DBに問い合わせてログインする。失敗したらnullを返す
+	public static MemberBean login(String userId,String password) {
+
+		List<MemberBean> members = new MemberDAO().executeQuery(conn -> {
+			PreparedStatement pStmt = conn.prepareStatement("SELECT * FROM member WHERE user_id = ? AND password = ?");
+			pStmt.setString(1, userId);
+			pStmt.setString(2, password);
+			return pStmt;
+		});
+
+		return members.isEmpty() ? null : members.get(0);
+
 	}
 }
