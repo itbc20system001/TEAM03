e15787f25721501cf7e1bd3f247045b2dddcde6a
kanagawa <kanagawa@MP613.mzdomain.maize.co.jp>
2020-05-28 11:43:45 +0900

    ItemDAOのカラム名間違いを修正 DAOのテストプログラム作成

--- WebProject/src/dao/DAOTester.java
+++ WebProject/src/dao/DAOTester.java
@@ -4,7 +4,10 @@ public class DAOTester {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		new ItemDAO().executeQuery(conn -> conn.prepareStatement("SELECT * FROM item"))
+			.stream()
+			.map(ob -> ob.getItemCd() +" " + ob.getItemText())
+			.forEach(System.out::println);
 	}
 
 }
