088451019433f8fc71924bdaf0aeed200f4d9c89
atsu-shi <atsu-shi@NA20428.nature.local>
2020-05-28 15:57:37 +0900

    ユーザーの購入履歴取得　後で単体のclassにする予定

--- WebProject/src/servlet/OrderCheck.java
+++ WebProject/src/servlet/OrderCheck.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +10,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.PurchaseHistoryBean;
 
 /**
  * Servlet implementation class OrderCheck
@@ -16,13 +21,13 @@ import javax.servlet.http.HttpServletResponse;
 public class OrderCheck extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public OrderCheck() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public OrderCheck() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -37,13 +42,30 @@ public class OrderCheck extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		boolean a = true;
+		HttpSession session = request.getSession();
+		String userId = (String) session.getAttribute("ユーザーを保存している奴の名前");
+		List<PurchaseHistoryBean> pHList = new ArrayList<>();
+		pHList = (List<PurchaseHistoryBean>) session.getAttribute("PurchaseHistoryList");
+		List<PurchaseHistoryBean> userPHList = new ArrayList<>();
+		for(PurchaseHistoryBean i:pHList) {
+			if(userId.equals(i.getUserId())){
+				userPHList.add(i);
+			}
+		}
 		/*
 		 処理内容
 		 1．購入履歴からの場合はスコープから取得した
 		    情報をOrderCheck.jspに投げる
+		 */
+		if(a) {
+
+		}else{/*
 		 2. customize.jspからの場合はスコープに格納されている
 		    情報を購入履歴のDBに保存してからOrderCheck.jspに投げる
 		 */
+		}
 		RequestDispatcher rdp = request.getRequestDispatcher("WEB-INF/jsp/orderCheck.jsp");
 		rdp.forward(request, response);
 	}
