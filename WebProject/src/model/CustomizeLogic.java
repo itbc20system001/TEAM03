1a45af7b32674d74b6fc38421ed017d90b97d800
atsu-shi <atsu-shi@NA20428.nature.local>
2020-05-28 11:50:19 +0900

    トッピング内容を取得するメソッド作成

--- WebProject/src/model/CustomizeLogic.java
+++ WebProject/src/model/CustomizeLogic.java
@@ -1,5 +1,20 @@
 package model;
 
+import java.util.List;
+
+import dao.CustomizeDAO;
+
 public class CustomizeLogic {
+
+	CustomizeDAO customizeDAO = new CustomizeDAO();
+
+	//ここで処理する内容はカスタマイズのDBからデータを持ってきてListに格納する処理
+	public List<CustomizeBean> makeCustomizeList(String customizeCalamName,String customizeCodeName, String tableName){
+		customizeDAO.setCustomizeCalamName(customizeCalamName);
+		customizeDAO.setCustomizeCodeName(customizeCodeName);
+		List<CustomizeBean> custimizeList = customizeDAO.executeQuery( conn -> conn.prepareStatement("SELECT * FROM "+tableName) );
+		return custimizeList;
+
+	}
 
 }
