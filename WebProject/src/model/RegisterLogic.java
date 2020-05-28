1ed563913bb43391cf250a21c3484af2d06f9238
haru-wa <haru-wa@NA19650.nature.local>
2020-05-28 14:49:39 +0900

    会員登録のロジックを作成（DB保存は未完）

---
+++ WebProject/src/model/RegisterLogic.java
@@ -0,0 +1,19 @@
+package model;
+
+//会員情報を登録し、成功すればtrueを返す
+public class RegisterLogic {
+
+	public boolean execute(MemberBean member) {
+		//daoのメソッドを呼び出す
+		//受け取るのはmemberでいいのか…？
+		//どうにかしてDBに情報を入れる
+		/*	if() {
+				return true;
+			} else {
+				return false;
+			}
+		*/ return true;
+
+	}
+
+}
--- WebProject/src/servlet/Item.java
+++ WebProject/src/servlet/Item.java
@@ -1,5 +1,4 @@
 package servlet;
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -12,24 +11,27 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Item")
 public class Item extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+//セッションスコープ（仮）にある個別アイテムの情報をとってくる
+//item.jspに投げる
     public Item() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//itemList.jspから処理が投げられる
+		//item.jspにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/item.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//item.jspからドリンクの値を受け取る（方法が分からん）
+
 
 		//ドリンクの値をスコープに保存
 
-		//customizeに処理を投げる
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/customize.jsp");
-		dispatcher.forward(request, response);
+		//customizeに処理を投げる　嘘。違った。
+		//RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/customize.jsp");
+		//dispatcher.forward(request, response);
 	}
 
 }
