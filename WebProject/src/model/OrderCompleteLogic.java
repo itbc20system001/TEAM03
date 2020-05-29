a29f52cae2d9efc03716e8973fb754c898b7293f
kanagawa <kanagawa@MP613.mzdomain.maize.co.jp>
2020-05-29 13:59:47 +0900

    注文完了処理のサーブレット部分完成

---
+++ WebProject/src/model/OrderCompleteLogic.java
@@ -0,0 +1,10 @@
+package model;
+
+//注文内容をDBに保存するクラス
+public class OrderCompleteLogic {
+
+	//memberの注文orderDateilを注文履歴に保存
+	public static void leaveOrder(OrderDetailBean orderDetail,MemberBean member) {
+
+	}
+}
--- WebProject/src/servlet/OrderComplete.java
+++ WebProject/src/servlet/OrderComplete.java
@@ -1,12 +1,19 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.MemberBean;
+import model.OrderCompleteLogic;
+import model.OrderDetailBean;
 
 /**
  * Servlet implementation class OrderComplete
@@ -19,11 +26,25 @@ public class OrderComplete extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		//商品をセッションスコープに入れた際の名前決まるまで保留
-		
+
+		HttpSession session = request.getSession();
+
+		//注文内容一覧をセッションスコープから取得
+		List<OrderDetailBean> orderList =  (List<OrderDetailBean>)session.getAttribute("orderList");
+
+		//ログインしているユーザーを取得
+		MemberBean member = (MemberBean)session.getAttribute("user");
+
+		//各注文内容を購入履歴に保存
+		orderList.forEach(od -> OrderCompleteLogic.leaveOrder(od, member));
+
+		//履歴に保存し終わったら注文内容一覧を空にする
+		session.setAttribute("orderList", new ArrayList<OrderDetailBean>());
+
+		//注文完了画面にフォワード
+		request.getRequestDispatcher("/WEB-INF/jsp/orderCoplete.jsp").forward(request, response);
+
+
 	}
 
 }
